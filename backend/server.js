8bea2bffa4e0090f1d5be3a26fdf306e49527e8d
T.Mounish Aniruth <Mounishaniruth1708@gmail.com>
2024-12-22 09:41:51 +0530

    delete

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes'); // Import authRoutes
+
+const app = express();
+
+
+// Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+// Use authRoutes for authentication-related routes
+app.use('/api', authRoutes); // All auth routes will have the `/api` prefix
+
+// Start server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
